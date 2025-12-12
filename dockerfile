77474af1a1bcf08a9c8548079efac6728ceee987
Joan <70104624+itsjovdev@users.noreply.github.com>
2025-12-12 23:38:03 +0100

    feat: se agrega los dockerfiles

---
+++ dockerfile
@@ -0,0 +1,22 @@
+
+
+FROM node:20-alpine
+
+
+WORKDIR /usr/src/app
+
+# Copiar archivos de dependencias
+COPY package.json ./
+COPY package-lock.json ./
+
+# Instalar dependencias
+RUN npm install
+
+# Copiar el resto del proyecto
+COPY . .
+
+# # Generar cliente de Prisma
+# RUN npx prisma generate
+
+# Exponer el puerto
+EXPOSE 3000
---
+++ src/config/envs.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+export const envSchema = z
+  .object({
+    PORT: z.string().min(1, 'PORT is required.').transform(Number),
+    ALLOWED_ORIGINS: z
+      .string()
+      .min(1, 'ALLOWED_ORIGINS is required.')
+      .transform((val) => val.split(',').map((origin) => origin.trim())),
+   
+  })
+  .passthrough();
+
+type envType = z.infer<typeof envSchema>;
+
+const envParsed = envSchema.safeParse(process.env);
+
+if (!envParsed.success) {
+  console.error('❌ Config validation error:', envParsed.error.format());
+  throw new Error('Invalid environment variables');
+}
+
+export const envs: envType = {
+  PORT: envParsed.data.PORT,
+  ALLOWED_ORIGINS: envParsed.data.ALLOWED_ORIGINS,
+
+};
+
